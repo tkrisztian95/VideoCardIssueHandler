298e388ed84925d28e62f01bab7e784ce8f66ea6
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2014-08-18 15:07:35 +0200

    Init beta

---
+++ VCardFreqChangerTool/VideoCardHandler/Common.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoCardHandler
+{
+    public static class Common
+    {
+
+        //----------------------------File Műveletek--------------------------//
+        public static bool createFile(string folderPath, string fileName, bool overwrite, string[] linesContent)
+        {
+
+            try
+            {
+                string fullFilePath = folderPath + @"\" + fileName;
+                if (Directory.Exists(folderPath) == false)
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                if (File.Exists(fullFilePath))
+                {
+
+                    if (overwrite)
+                    {
+
+                        File.Delete(fullFilePath);
+
+                    }
+                    else
+                    {
+
+                        return false;
+
+                    }
+
+
+
+                }
+
+                using (StreamWriter sw = new StreamWriter(fullFilePath, true))
+                {
+                    for (int i = 0; i < linesContent.Length; i++)
+                    {
+
+                        sw.WriteLine(linesContent[i]);
+
+
+                    }
+
+
+                }
+
+                return true;
+            }
+            catch
+            {
+
+
+                return false;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ VCardFreqChangerTool/VideoCardHandler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoCardHandler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+#if DEBUG
+
+            ClockToolService Service = new ClockToolService();
+            Service.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new ClockToolService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
